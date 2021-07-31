a3c2d0f80abcbaec8faecf75e698d8ec6c9ca64d
Ed <ed@polarity.io>
2021-07-30 19:15:50 -0700

    Fix web url if it contains a trailing slash

--- components/block.js
+++ components/block.js
@@ -1,3 +1,7 @@
 polarity.export = PolarityComponent.extend({
-    details: Ember.computed.alias('block.data.details')
+    details: Ember.computed.alias('block.data.details'),
+    webUrl: Ember.computed('block.userOptions.url', function(){
+        const url = this.get('block.userOptions.url');
+        return url.endsWith('/') ? url : `${url}/`;
+    })
 });
